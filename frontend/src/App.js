6540190b2c3e1ed22c321628a16c7943c3692c58
kritikachaubey04 <kritikachaubey04@gmail.com>
2026-09-05 00:56:10 +0530

    Connect frontend to live Render API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE = "http://localhost:8000/api";
+const API_BASE = "https://https://employee-task-system-9kzh.onrender.com/api";
 
 export default function App() {
   const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem('session_user')) || null);
